1d06f32ba3dd3a15766a04c09f288d8e641045e3
Melissa Quinnan <mkq5008@gmail.com>
2022-05-23 13:08:33 +0200

    asttbar

--- plots/PlotOutput.py
+++ plots/PlotOutput.py
@@ -131,7 +131,7 @@ class PlotOutput:
                 "fj_ttbar_bmerged",
                 "fj_wjets_label",
             ]
-	elif "as_t" in self.bkg:
+	elif "asttbar" in self.bkg:
             bkglabels = [
                 "fj_ttbar_bsplit",
                 "fj_ttbar_bmerged",
